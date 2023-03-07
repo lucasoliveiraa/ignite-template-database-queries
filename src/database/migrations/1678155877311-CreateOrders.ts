1e64939f7067f72799ca2b3078fb04a3dc188287
lucasoliveiraa <lucasjackcosta18@gmail.com>
2023-03-06 23:26:30 -0300

    ✨ finish

---
+++ src/database/migrations/1678155877311-CreateOrders.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateOrders1678155877311 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "orders",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "total",
+            type: "float",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("orders");
+  }
+}
